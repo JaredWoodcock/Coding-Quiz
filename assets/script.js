46743523adfdaf25124e555295d6670e204173d0
Jared Woodcock <jared.woodcock@yahoo.com>
2023-10-17 18:58:03 -0500

    First Commit

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+var timer = document.getElementById('timer');
+var time = 60;
+
+var questions = [
+    {
+        question: "1",
+        options: ["1", "2", "3", "4"],
+        answer: 1
+    },
+    {
+        question: "1",
+        options: ["1", "2", "3", "4"],
+        answer: 1
+    },
+    {
+        question: "1",
+        options: ["1", "2", "3", "4"],
+        answer: 1
+    },
+    {
+        question: "1",
+        options: ["1", "2", "3", "4"],
+        answer: 1
+    },
+    {
+        question: "1",
+        options: ["1", "2", "3", "4"],
+        answer: 1
+    },
+    {
+        question: "1",
+        options: ["1", "2", "3", "4"],
+        answer: 1
+    },
+];
+
+
+function startQuiz() {
+    var
+}
